70d0917a879c39bee3fa642db1ebd41b3775d9f5
Carlos José Ruiz-Henestrosa Ruiz <ruizh.cj@gmail.com>
2020-08-23 23:44:17 +0200

    Corrected typos

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;;; Code:
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
